59983c248970cc724211b669be1565fe9d1b69c8
Izac <izikki11499@gmail.com>
2022-08-16 20:36:55 -0700

    added some basic code, mainly the book object constructor and some placholder function for adding a book to the library

---
+++ app.js
@@ -0,0 +1,16 @@
+//All book objects stored in this array
+let myLibrary = [];
+
+//book constructor
+function Book(title, author, pages, status){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+//add a book to Library
+function addBook(){
+    //do the stuff here
+}
+
