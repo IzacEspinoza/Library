348b4bdf88a5e644ed8a5443698d2fc7cffbdf35
Izac <izikki11499@gmail.com>
2022-08-19 23:11:25 -0700

    Just messing with the addBook(), trying to get basic working function working and go from there

--- app.js
+++ app.js
@@ -12,5 +12,20 @@ function Book(title, author, pages, status){
 //add a book to Library
 function addBook(){
     //take user’s input 
+    let info = prompt('stuff?');
     //store the new book objects into myLibrary[]
+    myLibrary.push(info);
+
+
 }
+
+//test our constructor n stuff
+let book1 = new Book('Witcher', 'Geralt', 200, 'read');
+// console.log(book1);
+let book2 = new Book('Ghost recon', 'Tom Clancy', 654, 'not read');
+
+//testing our add book function
+for(let i = 0; i < 4; i++){
+    addBook();
+}
+console.log(myLibrary);
